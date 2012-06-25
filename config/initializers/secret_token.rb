d0747d7c917bbd7083a635c8ea9b32994aed1ba7
Rob McAleavy <rob@cardscreative.com>
2012-06-25 08:47:50 -0600

    Original first Twitter Research files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterResearch::Application.config.secret_token = '0c1d57a740f74449b56628119a961071bfe5a95d94d835cb217ca77be8d53d55443821567ab6d3a277644ac300773d09ba745574a0834aa92155f66559166c96'
