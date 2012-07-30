9a0d439e3c768488fc96efc8005bede4aa42d61a
Rob McAleavy <rob@cardscreative.com>
2012-07-30 07:47:15 -0600

    Added/corrected categories controller, model & tweet model

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,36 +1,44 @@
 class CategoriesController < ApplicationController
 
+  # Show a list of all categories
   def index
     @categories = Category.order(:title).limit(50)
     respond_with([@categories, @title])
   end
 
+  # Show a single category with all tweets
   def show
     @category = Category.includes(:tweets).
       order('tweets_at desc').find(params[:id])
     respond_with(@category)
   end
 
+  # Show form for a new category
   def new
     @category = Category,new
     respond_with(@category)
   end
 
+  # Save/create the new category with new params
   def create
     @category = Category.create(params[:category])
     respond_with(@category)
   end
 
+  # Show a form to let users edit form submitted
   def edit
+    @category = Category.find(params[:id])
     respond_with(@category)
   end
 
+  # Update the category after the edit form is submitted
   def update
     @category = Category.find(params[:id])
     @category.update_attributes(params[:category])
     respond_with(@category)
   end
 
+  # Delete a category
   def destroy
     @category = Category.find(params[:id])
     @category.destroy
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,37 +3,39 @@ class Tweet < ActiveRecord::Base
 
   validates(:twitter_user, :presence => true)
   validates(:tweeted_text, :presence => true)
-  validates(:tweeted_at, :presence => true)
+  validates(:twitter_at, :presence => true)
 
-  scope(:with_notes, includes(:notes).order('notes.created_at desc'))
-
-  belongs_to(:user)
-  has_and_belongs_to_many(:categories)
-  has_many(:notes)
-
-  def suggested_categories
-    titles = Category.all.map(&:title).map(&:downcase)
-    tweet = tweeted_text.gsub(/[^\w\s]/, ' ').downcase.split.sort
+  scope(:with_notes, includes(:notes.order('notes.created_at desc')))
 
 
-    (tweet & titles).map(&:capitalize).sort.join(', ')
-  end
-  ##############################################################
-  # Returns comma separated string of the current category titles.
-  def categories_as_string
-    categories.map(&:title).sort.join(', ')
+        belongs_to(:user)
+        has_many(:notes, :dependent, :delete_all)
+        has_and_belongs_to_many(:categories)
 
-  end
-  ###########################################################################
-  # Given a comma separated string of category titles, reset the
-  # categories for this tweet to the categories in the string.
+        def suggested_categories
+          titles = Category.all.map(&:title).map(&:downcase)
+          tweet = tweeted_text.gsub(/[^\w\s]/, ' ').downcase.split.sort
 
-  def categories_as_string= (new_categories)
-    categories.clear
-    new_categories.split(/\s*,\s*/).each do |title|
-      cat = Category.where('LOWER(title) = ?', title.downcase).first
-      categories << cat if !cat.nil?
-    end
-  end
-end
+
+          (tweet & titles).map(&:capitalize).sort.join(', ')
+        end
+
+        ##############################################################
+        # Returns comma separated string of the current category titles.
+        def categories_as_string
+          categories.map(&:title).sort.join(', ')
+
+        end
+        ###########################################################################
+        # Given a comma separated string of category titles, reset the
+        # categories for this tweet to the categories in the string.
+
+        def categories_as_string= (new_categories)
+          categories.clear
+          new_categories.split(/\s*,\s*/).each do |title|
+            cat = Category.where('LOWER(title) = ?', title.downcase).first
+            categories << cat if !cat.nil?
+          end
+        end
+      end
 
