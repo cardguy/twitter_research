363e78c833be67539880b8f7a537baa4b67fa4f2
Rob McAleavy <rob@cardscreative.com>
2012-08-06 07:16:16 -0600

    Catching up and trying to add back categories and notes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  respond_to(:html, :json)
+  respond_to(:html, :json, :js)
   before_filter(:authenticate_user!)
 
 end
